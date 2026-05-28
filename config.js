13f8274558c3274283f3dbf57520bde2b8cd06db
Edgardo Sandoval <edgardo.sandoval79@gmail.com>
2026-05-28 09:10:32 -0300

    Set API_URL to Google Apps Script deployment URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // [ARCHIVO 5] - config.js
 // URL del despliegue de Google Apps Script (Web App). Dejar vacío "" para usar el modo Demo Local.
-const API_URL = ""; 
+const API_URL = "https://script.google.com/macros/s/AKfycbzYpD51eOjbLVVvO9n4jMjy9_vAI9QJBI4e0RO8rCE_-odYLVyrbW9vi_P-yCC6Jq7p/exec"; 
 const APP_VERSION = "1.0";
 
 // Credenciales de acceso simples solicitadas
